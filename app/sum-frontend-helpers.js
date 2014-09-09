c680cee90a5e09395dc74ac7590a5fb1c85ff6d4
Tobias Zeising <tobias.zeising@aditu.de>
2014-09-09 11:35:39 +0200

    fix jshint warning

--- app/sum-frontend-helpers.js
+++ app/sum-frontend-helpers.js
@@ -15,7 +15,7 @@ define('sum-frontend-helpers', Class.extend({
         date = new Date(date);
         var day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
         var month = date.getMonth() + 1;
-        var month = month < 10 ? "0" + month : month;
+        month = month < 10 ? "0" + month : month;
         var year = date.getFullYear();
         var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
         var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
